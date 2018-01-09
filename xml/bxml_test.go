c746aae2efff1da57abbe80741e18f48eaa3befe
Andrey Belchikov <belchikovs@gmail.com>
2018-01-09 10:06:49 +0300

    Move bxml test to xml

--- xml/bxml_test.go
+++ xml/bxml_test.go
@@ -1,14 +1,12 @@
-package bandwidth
+package xml
 
 import (
 	"encoding/xml"
 	"testing"
-
-	bxml "./xml"
 )
 
 func TestToXML(t *testing.T) {
-	response := &bxml.Response{}
+	response := &Response{}
 	expect(t, response.ToXML(), `<Response></Response>`)
 }
 
@@ -16,7 +14,7 @@ func TestToXMLWithSimpleVerb(t *testing.T) {
 	type Test struct {
 		XMLName xml.Name `xml:"Test"`
 	}
-	response := &bxml.Response{Verbs: []interface{}{Test{}}}
+	response := &Response{Verbs: []interface{}{Test{}}}
 	expect(t, response.ToXML(), `<Response><Test></Test></Response>`)
 }
 
@@ -27,7 +25,7 @@ func TestToXMLWithMultipleVerbs(t *testing.T) {
 	type Test2 struct {
 		XMLName xml.Name `xml:"Test2"`
 	}
-	response := &bxml.Response{Verbs: []interface{}{Test1{}, Test2{}}}
+	response := &Response{Verbs: []interface{}{Test1{}, Test2{}}}
 	expect(t, response.ToXML(), `<Response><Test1></Test1><Test2></Test2></Response>`)
 }
 
@@ -38,26 +36,26 @@ func TestToXMLWithDefaultValues(t *testing.T) {
 		Field2  interface{} `xml:"field2,attr,omitempty"`
 		Field3  interface{} `xml:"field3,attr,omitempty"`
 	}
-	response := &bxml.Response{Verbs: []interface{}{Test{}}}
+	response := &Response{Verbs: []interface{}{Test{}}}
 	expect(t, response.ToXML(), `<Response><Test></Test></Response>`)
-	response = &bxml.Response{Verbs: []interface{}{Test{Field1: "value1", Field2: 11, Field3: false}}}
+	response = &Response{Verbs: []interface{}{Test{Field1: "value1", Field2: 11, Field3: false}}}
 	expect(t, response.ToXML(), `<Response><Test field1="value1" field2="11" field3="false"></Test></Response>`)
-	response = &bxml.Response{Verbs: []interface{}{Test{Field1: "value2", Field2: 12, Field3: true}}}
+	response = &Response{Verbs: []interface{}{Test{Field1: "value2", Field2: 12, Field3: true}}}
 	expect(t, response.ToXML(), `<Response><Test field1="value2" field2="12" field3="true"></Test></Response>`)
 }
 
 func TestVerbs(t *testing.T) {
-	response := &bxml.Response{Verbs: []interface{}{
-		bxml.Gather{RequestURL: "url"},
-		bxml.Pause{Duration: 10},
-		bxml.Hangup{},
-		bxml.PlayAudio{URL: "url"},
-		bxml.Record{RequestURL: "url"},
-		bxml.Redirect{RequestURL: "url"},
-		bxml.Reject{Reason: "none"},
-		bxml.SendMessage{From: "from", To: "to", Text: "text"},
-		bxml.SpeakSentence{Sentence: "Hello"},
-		bxml.Transfer{TransferTo: "number", SpeakSentence: &bxml.SpeakSentence{Sentence: "Please wait"}},
+	response := &Response{Verbs: []interface{}{
+		Gather{RequestURL: "url"},
+		Pause{Duration: 10},
+		Hangup{},
+		PlayAudio{URL: "url"},
+		Record{RequestURL: "url"},
+		Redirect{RequestURL: "url"},
+		Reject{Reason: "none"},
+		SendMessage{From: "from", To: "to", Text: "text"},
+		SpeakSentence{Sentence: "Hello"},
+		Transfer{TransferTo: "number", SpeakSentence: &bxml.SpeakSentence{Sentence: "Please wait"}},
 	}}
 	expect(t, response.ToXML(), `<Response><Gather requestUrl="url"></Gather><Pause duration="10"></Pause><Hangup></Hangup><PlayAudio>url</PlayAudio><Record requestUrl="url"></Record><Redirect requestUrl="url"></Redirect><Reject reason="none"></Reject><SendMessage from="from" to="to">text</SendMessage><SpeakSentence>Hello</SpeakSentence><Transfer transferTo="number"><SpeakSentence>Please wait</SpeakSentence></Transfer></Response>`)
 }
